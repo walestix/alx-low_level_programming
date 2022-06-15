cc034030e5bbb2c8545b8326185cc01383ab65ad
walestix <okikiwale11@gmail.com>
2022-06-14 17:06:06 -0700

    New repo

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - generates keygen.
- *   * Return: 0 Always.
+ * main - generates keygen.
+ * Return: 0 Always.
  */
 int main(void)
 {
 	int r = 0, c = 0;
 	time_t t;
-
 	srand((unsigned int) time(&t));
 	while (c < 2772)
 	{
